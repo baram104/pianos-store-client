077acb06115fba25d16e39e49ac1589cea1cb3bf
baram104 <baram104@gmail.com>
2022-07-05 12:38:03 +0300

    added wish list page to my account

---
+++ src/MyWishList.js
@@ -0,0 +1,13 @@
+import ProductDetails from "./ProductDetails";
+
+export default function MyWishList() {
+  return (
+    <div className="container">
+      <ProductDetails />
+      <ProductDetails />
+      <ProductDetails />
+      <ProductDetails />
+      <ProductDetails />
+    </div>
+  );
+}
