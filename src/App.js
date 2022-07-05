9dcedd0d848de2faa272fc4efa89e788819ea4c6
baram104 <baram104@gmail.com>
2022-07-05 12:34:54 +0300

    product details has been modified and divided into fewer components.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      {/* <ProductDetails /> */}
+      <ProductDetails />
       <MyAccount />
       <Footer />
     </div>
--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,3 +1,5 @@
+import ProductButtonGroup from "./ProductButtonGroup";
+
 function ProductDetails() {
   return (
     <section className="container">
@@ -20,23 +22,7 @@ function ProductDetails() {
                   <small className="card-text">19$</small>
                 </p>
               </div>
-              <div className="col-lg-3 text-center">
-                <div className="input-group mt-3 justify-content-center">
-                  <span className="input-group-text" id="basic-addon1">
-                    Qty
-                  </span>
-                  <input
-                    style={{ maxWidth: "4vw" }}
-                    type="number"
-                    className="form-control"
-                    aria-label="Quantity"
-                    aria-describedby="basic-addon1"
-                  />
-                </div>
-                <button className="btn btn-dark mt-3">Buy it now</button>
-                <button className="btn btn-primary mt-3">Add to cart</button>
-                <button className="btn btn-danger mt-3">Add to wishlist</button>
-              </div>
+              <ProductButtonGroup />
             </div>
           </div>
         </div>
