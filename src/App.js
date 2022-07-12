77ed7a110273fd73d1d55815bf6ebfae4bae93b3
baram104 <baram104@gmail.com>
2022-07-12 10:37:37 +0300

    finished structuring the component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import ProductDetails from "./ProductDetails/ProductDetails";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import MyAccount from "./MyAccount/MyAccount";
+import LoginForm from "./LoginForm";
 
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <ProductDetails isOnWishList={false} isOutOfStock={false} />
+      {/* <ProductDetails isOnWishList={false} isOutOfStock={false} /> */}
       {/* <MyAccount /> */}
+      <LoginForm />
       <Footer />
     </div>
   );
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,10 +1,65 @@
 import { Form } from "react-bootstrap";
+import "./LoginForm.css";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 import Button from "react-bootstrap/esm/Button";
+import { useState } from "react";
 
-export default function LoginForm() {
+export default function LoginForm(props) {
+  const [loginForm, setLoginForm] = useState({
+    username: {
+      value: "",
+      validations: {
+        required: true,
+        minLength: 2,
+      },
+      errors: [],
+    },
+    password: {
+      value: "",
+      validations: {
+        required: true,
+        pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      },
+      errors: [],
+    },
+  });
+
   return (
-    <Form>
-      <h2>Sign In</h2>
+    <Form className="container w-50 mt-5 card">
+      <Row>
+        <h2>Sign In</h2>
+        <div className="line"></div>
+      </Row>
+      <Row>
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            name="username"
+            type="text"
+            placeholder="Username"
+            isInvalid={false}
+            defaultValue={loginForm.username.value}
+          />
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+      </Row>
+      <Row className="my-5 justify-content-center">
+        <Col md={4}>
+          <Button className="w-100" type="submit">
+            Submit
+          </Button>
+        </Col>
+      </Row>
     </Form>
   );
 }
