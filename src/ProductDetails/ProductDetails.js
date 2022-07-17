a4bc666b1a5d7f6cd80ce7bb70d2814a0e64eb69
baram104 <baram104@gmail.com>
2022-07-17 16:46:32 +0300

    integrated api get requests using the demo data

--- src/ProductDetails/ProductDetails.js
+++ src/ProductDetails/ProductDetails.js
@@ -2,84 +2,95 @@ import OutOfStockButtons from "./OutOfStockButtons";
 import filledHeart from "../filledheart.png";
 import emptyHeart from "../emptyheart.png";
 import styles from "./ProductDetails.module.css";
+import { useEffect, useState } from "react";
+import * as api from "../DAL/api";
+import Spinner from "react-bootstrap/Spinner";
 
 function ProductDetails(props) {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    api.getPiano(props.id).then((data) => setProduct(data));
+  }, []);
+
   return (
     <section className="container">
-      <div className="card mt-3">
-        <div className="row g-0">
-          <div className="col-lg-5 col-md-12">
-            <img
-              style={{ height: "100%", width: "100%" }}
-              src="https://i.ytimg.com/vi/7SSu0KtXI2c/maxresdefault.jpg"
-              className="img-fluid rounded-start"
-              alt="..."
-            />
-          </div>
-          <div className="col-lg-7 col-md-12">
-            <div className="card-body row">
-              <div className="col text-start">
-                <h3 className="card-title text-dark">Product title</h3>
-                <p className="card-text text-primary">
-                  <h3>$19</h3>
-                </p>
-                <p className="card-text">
-                  Laboris exercitation ea eu culpa et officia. Proident mollit
-                  culpa velit id quis eu. Dolor tempor irure cillum amet
-                  reprehenderit eiusmod incididunt.
-                </p>
-                <div className="col text-center">
-                  {props.isOutOfStock ? (
-                    <OutOfStockButtons />
-                  ) : (
-                    <div>
-                      <button className="btn btn-dark mt-3 rounded-0">
-                        Buy it now
-                      </button>
-                    </div>
-                  )}
-                  {props.isOutOfStock ? (
-                    ""
-                  ) : (
-                    <div className="col justify-content-center mt-3">
-                      <div className="input-group justify-content-center">
-                        <span className="input-group-text" id="basic-addon1">
-                          Qty
-                        </span>
-                        <input
-                          style={{ maxWidth: "4vw" }}
-                          type="number"
-                          className="form-control"
-                          aria-label="Quantity"
-                          aria-describedby="basic-addon1"
-                        />
-                        <button className="btn btn-primary mx-3 rounded-0">
-                          Add to cart
+      {product ? (
+        <div className="card mt-3">
+          <div className="row g-0">
+            <div className="col-lg-5 col-md-12">
+              <img
+                style={{ height: "100%", width: "100%" }}
+                src={product.imgSrc}
+                className="img-fluid rounded-start"
+                alt={product.imgAlt}
+                title={product.imgTitle}
+              />
+            </div>
+            <div className="col-lg-7 col-md-12">
+              <div className="card-body row">
+                <div className="col text-start">
+                  <h3 className="card-title text-dark">{product.name}</h3>
+                  <p className="card-text text-primary">
+                    <h3>${product.price}</h3>
+                  </p>
+                  <p className="card-text">{product.description}</p>
+                  <div className="col text-center">
+                    {!product.quantity ? (
+                      <OutOfStockButtons />
+                    ) : (
+                      <div>
+                        <button className="btn btn-dark mt-3 rounded-0">
+                          Buy it now
                         </button>
-                        {props.isOnWishList ? (
-                          <span className={styles.wishlistIconContainer}>
-                            <img
-                              className={styles.wishlistIcon}
-                              src={filledHeart}
-                            ></img>
-                          </span>
-                        ) : (
-                          <span className={styles.wishlistIconContainer}>
-                            <img
-                              className={styles.wishlistIcon}
-                              src={emptyHeart}
-                            ></img>
-                          </span>
-                        )}
                       </div>
-                    </div>
-                  )}
+                    )}
+                    {product.quantity && (
+                      <div className="col justify-content-center mt-3">
+                        <div className="input-group justify-content-center">
+                          <span className="input-group-text" id="basic-addon1">
+                            Qty
+                          </span>
+                          <input
+                            style={{ maxWidth: "4vw" }}
+                            type="number"
+                            min={1}
+                            className="form-control"
+                            aria-label="Quantity"
+                            max={product.quantity}
+                          />
+                          <button className="btn btn-primary mx-3 rounded-0">
+                            Add to cart
+                          </button>
+                          {props.isOnWishList ? (
+                            <span className={styles.wishlistIconContainer}>
+                              <img
+                                className={styles.wishlistIcon}
+                                src={filledHeart}
+                              ></img>
+                            </span>
+                          ) : (
+                            <span className={styles.wishlistIconContainer}>
+                              <img
+                                className={styles.wishlistIcon}
+                                src={emptyHeart}
+                              ></img>
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="d-flex justify-content-center">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      )}
     </section>
   );
 }
