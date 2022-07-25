7fb74c5d3eadb8e5b74dbc304333f16a54ff4342
baram104 <baram104@gmail.com>
2022-07-25 16:39:04 +0300

    changed a bit the structure of the checkout page

--- src/CheckoutPage.js
+++ src/CheckoutPage.js
@@ -1,4 +1,4 @@
-import { Container, Row, Card, Button } from "react-bootstrap";
+import { Container, Row, Card, Button, Col } from "react-bootstrap";
 import { formInputs } from "./data/formInputsData";
 import CartProduct from "./CartProduct";
 import FormComp from "./FormComp";
@@ -13,24 +13,27 @@ export default function CheckoutPage() {
     <Container as={Card} className="p-5 my-3">
       <Row className="mx-5">
         <h2>Checkout</h2>
-        <h2 className="text-primary my-3">Summary $1200</h2>
       </Row>
       <Row>
-        <CartProduct />
-        <CartProduct />
-        <CartProduct />
+        <Col>
+          <CartProduct />
+          <CartProduct />
+          <CartProduct />
+        </Col>
+        <Col>
+          <FormComp
+            formTitle="Ship To"
+            isCheckout={true}
+            handleFormDetails={() => {}}
+            formInputs={addressForm}
+          />
+          <h2 className="text-primary my-3 text-center">Summary $1200</h2>
+          <div className="justify-content-center d-flex">
+            <Button>Place Order</Button>
+          </div>
+        </Col>
       </Row>
-      <Row>
-        <FormComp
-          formTitle="Ship To"
-          isCheckout={true}
-          handleFormDetails={() => {}}
-          formInputs={addressForm}
-        />
-      </Row>
-      <Row md={2} className="justify-content-center d-flex">
-        <Button>Place Order</Button>
-      </Row>
+      <Row md={2} className="justify-content-center d-flex"></Row>
     </Container>
   );
 }
