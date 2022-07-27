54cae65df26b3abb016e9aa4d36fc0336aac7e5e
baram104 <baram104@gmail.com>
2022-07-27 15:46:29 +0300

    use party js in order summary page

--- src/Pages/OrderSummary/OrderSummaryPage.js
+++ src/Pages/OrderSummary/OrderSummaryPage.js
@@ -1,8 +1,16 @@
 import { Card, Container } from "react-bootstrap";
+import party from "party-js";
+import { useEffect, useRef } from "react";
 
 export default function OrderSummaryPage() {
+  const containerRef = useRef();
+  useEffect(() => {
+    party.confetti(containerRef.current, {
+      count: party.variation.range(20, 40),
+    });
+  }, []);
   return (
-    <Container className="mt-5">
+    <Container ref={containerRef} className="mt-5">
       <Card>
         <Card.Header className="text-primary">
           <h1>Your Order has been placed!</h1>
